45a7f03620b83d0b28f3fafd6490ff8647ded021
Aleksandr Chashchin <achasch@gmail.com>
2024-06-03 17:42:35 +0200

    add drag

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import './App.css'
+import { createSignal } from 'solid-js';
+import './App.css';
 
 export function App() {
+  const [file, setFile] = createSignal<File>();
+  const [isDragOver, setIsDragOver] = createSignal(false);
+
+  function onDrop(e: DragEvent) {
+    e.preventDefault();
+    const file = e.dataTransfer?.files[0];
+    if (file) {
+      setFile(file);
+    }
+  }
+
+  function onDragOver(e: DragEvent) {
+    e.preventDefault();
+    setIsDragOver(true);
+  }
+
+  function onDragLeave() {
+    setIsDragOver(false);
+  }
+
   return (
-    <div class="App">
+    <div class="app">
+      <div
+        class="image"
+        classList={{ 'drag-over': isDragOver() }}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+      >
+        Drop image here
+      </div>
     </div>
   )
 }
