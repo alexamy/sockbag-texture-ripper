3481e697a4d166c8da69381eeddac8529d0f7ab3
Aleksandr Chashchin <achasch@gmail.com>
2024-06-03 20:22:31 +0200

    add rect

--- src/editor.ts
+++ src/editor.ts
@@ -1,32 +1,66 @@
 import { assign, setup } from "xstate";
 
 type Point = [number, number];
+type Rect = [Point, Point, Point, Point];
+
+type Context = {
+  current: Point;
+  points: Point[];
+  rectangles: Rect[];
+};
 
 export const editorMachine = setup({
   types: {
-    context: {} as { current: Point; points: Point[] },
+    context: {} as Context,
     events: {} as { type: "move"; point: Point } | { type: "click" },
   },
 }).createMachine({
   id: "editor",
-  initial: "initial",
+  initial: "clicking",
   context: {
+    rectangles: [],
     points: [],
     current: [0, 0],
   },
   on: {
-    click: {
-      actions: [
-        assign(({ context }) => ({
-          points: [...context.points, context.current],
-        })),
-      ],
-    },
-    move: {
-      actions: [assign(({ event }) => ({ current: event.point }))],
-    },
+    move: [
+      {
+        guard: ({ context }) => context.points.length === 4,
+        target: ".addRect",
+      },
+      {
+        actions: [assign(({ event }) => ({ current: event.point }))],
+      },
+    ],
   },
   states: {
-    initial: {},
+    clicking: {
+      on: {
+        click: {
+          actions: [
+            assign(({ context }) => ({
+              points: [...context.points, context.current],
+            })),
+          ],
+        },
+      },
+    },
+    addRect: {
+      always: {
+        target: "clicking",
+        actions: assign(({ context }) => ({
+          rectangles: [
+            ...context.rectangles,
+            [
+              context.points[0],
+              context.points[1],
+              context.points[2],
+              context.points[3],
+            ],
+          ],
+          points: [],
+        })),
+      },
+    },
   },
 });
