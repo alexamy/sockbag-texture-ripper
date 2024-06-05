286202f33b40d42182b561a4dbd02a77fb4d1189
Aleksandr Chashchin <achasch@gmail.com>
2024-06-05 18:02:06 +0200

    extract

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import potpack from "potpack";
 import {
-  For,
   Match,
   Show,
   Switch,
   createEffect,
   createMemo,
   createSignal,
-  on,
+  on
 } from "solid-js";
 import "./App.css";
 import { Editor } from "./Editor";
-import { Point, editorMachine } from "./editorMachine";
+import { Texture } from './Texture';
+import { editorMachine } from "./editorMachine";
 import { createActor, createActorState } from "./hooks";
 import { projectRectangles } from "./projection";
 
@@ -77,65 +76,6 @@ export function App() {
           </div>
         </Match>
       </Switch>
-    </div>
-  );
-}
-
-function Texture(props: { blobs: Blob[] }) {
-  const urls = createMemo(() => {
-    return props.blobs.map((blob) => URL.createObjectURL(blob));
-  });
-
-  const refs: HTMLImageElement[] = [];
-
-  const [positions, setPositions] = createSignal<Point[]>([]);
-  const transforms = createMemo(() => {
-    return positions().map(({ x, y }) => `translate(${x}px, ${y}px)`);
-  });
-
-  // add new rects to (0, 0)
-  createEffect(
-    on(
-      () => props.blobs.length,
-      () => {
-        const initialPositions = props.blobs
-          .map(() => ({ x: 0, y: 0 }))
-          .slice(positions().length);
-        setPositions([...positions(), ...initialPositions]);
-      }
-    )
-  );
-
-  function onAutoPack() {
-    const sizes = refs.map((ref, i) => {
-      const { width, height } = ref.getBoundingClientRect();
-      return { i, w: width, h: height, x: 0, y: 0 };
-    });
-
-    potpack(sizes);
-    sizes.sort((a, b) => a.i - b.i);
-
-    setPositions(sizes);
-  }
-
-  function onDownload() {}
-
-  return (
-    <div>
-      <button onClick={onAutoPack}>Autopack</button>
-      <button onClick={onDownload}>Download</button>
-      <div class="texture">
-        <For each={urls()}>
-          {(url, i) => (
-            <img
-              ref={refs[i()]}
-              class="texture-rect"
-              style={{ transform: transforms()[i()] }}
-              src={url}
-            />
-          )}
-        </For>
-      </div>
     </div>
   );
 }
---
+++ src/Texture.tsx
@@ -0,0 +1,62 @@
+import potpack from 'potpack';
+import { For, createEffect, createMemo, createSignal, on } from 'solid-js';
+import { Point } from './editorMachine';
+
+export function Texture(props: { blobs: Blob[] }) {
+  const urls = createMemo(() => {
+    return props.blobs.map((blob) => URL.createObjectURL(blob));
+  });
+
+  const refs: HTMLImageElement[] = [];
+
+  const [positions, setPositions] = createSignal<Point[]>([]);
+  const transforms = createMemo(() => {
+    return positions().map(({ x, y }) => `translate(${x}px, ${y}px)`);
+  });
+
+  // add new rects to (0, 0)
+  createEffect(
+    on(
+      () => props.blobs.length,
+      () => {
+        const initialPositions = props.blobs
+          .map(() => ({ x: 0, y: 0 }))
+          .slice(positions().length);
+        setPositions([...positions(), ...initialPositions]);
+      }
+    )
+  );
+
+  function onAutoPack() {
+    const sizes = refs.map((ref, i) => {
+      const { width, height } = ref.getBoundingClientRect();
+      return { i, w: width, h: height, x: 0, y: 0 };
+    });
+
+    potpack(sizes);
+    sizes.sort((a, b) => a.i - b.i);
+
+    setPositions(sizes);
+  }
+
+  function onDownload() {}
+
+  return (
+    <div>
+      <button onClick={onAutoPack}>Autopack</button>
+      <button onClick={onDownload}>Download</button>
+      <div class="texture">
+        <For each={urls()}>
+          {(url, i) => (
+            <img
+              ref={refs[i()]}
+              class="texture-rect"
+              style={{ transform: transforms()[i()] }}
+              src={url}
+            />
+          )}
+        </For>
+      </div>
+    </div>
+  );
+}
