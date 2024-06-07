fd000aff42056cfe9879104163e342533392ae28
Aleksandr Chashchin <achasch@gmail.com>
2024-06-07 19:37:32 +0200

    extract store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,92 +1,9 @@
-import {
-  JSXElement,
-  createContext,
-  createEffect,
-  on,
-  useContext,
-} from "solid-js";
-import { createStore } from "solid-js/store";
-import { createImageSource } from "../helper";
-import { v } from "../vector";
+import { JSXElement, createContext, useContext } from "solid-js";
+import { Store, createAppStore } from "./app";
 
+// TODO move to other place
 export type Point = { x: number; y: number };
 export type Quad = [Point, Point, Point, Point];
-
-type Store = ReturnType<typeof createAppStore>;
-interface StoreData {
-  file: Blob;
-  url: string;
-  image: HTMLImageElement;
-  points: Point[];
-  quads: Quad[];
-}
-
-// store
-function createAppStore() {
-  const [store, setStore] = createStore<StoreData>(getDefaultStore());
-
-  // TODO extract methods
-
-  // reset state when file changes
-  // prettier-ignore
-  createEffect(on(
-    () => store.file,
-    () => setStore(store => ({ ...getDefaultStore(), file: store.file }),
-  )));
-
-  // load image
-  // prettier-ignore
-  createEffect(on(
-    () => store.file,
-    async (file) => {
-      const url = URL.createObjectURL(file);
-      const image = await createImageSource(url);
-      setStore({ url, image });
-    },
-  ));
-
-  // add quad when has 4 points
-  // prettier-ignore
-  createEffect(on(
-    () => store.points,
-    (points) => {
-      if(points.length < 4) return;
-      const [p1, p2, p3, p4] = points;
-      const quad: Quad = [p1, p2, p3, p4];
-      const quads = [...store.quads, quad];
-      setStore({ quads, points: [] });
-    }
-  ));
-
-  function addPoint(point: Point) {
-    const isEqualSome = store.points.some((p) => v.equals(p, point));
-    const points = isEqualSome ? store.points : [...store.points, point];
-    setStore({ points });
-  }
-
-  function deleteLastPoint() {
-    const points = store.points.slice(0, -1);
-    setStore({ points });
-  }
-
-  function setFile(file: Blob) {
-    setStore({ file });
-  }
-
-  const methods = { setFile, addPoint, deleteLastPoint };
-
-  return [store, methods, setStore] as const;
-}
-
-function getDefaultStore() {
-  return {
-    url: "",
-    file: new Blob(),
-    image: new Image(),
-    points: [],
-    quads: [],
-  } satisfies StoreData;
-}
 
 // context
 const StoreContext = createContext<Store>(undefined as unknown as Store);
