9e47eb70437096b4ef67c6109f1d355d0c6a72cc
Aleksandr Chashchin <achasch@gmail.com>
2024-06-10 17:46:57 +0200

    remove bool flag

--- src/createDnd.ts
+++ src/createDnd.ts
@@ -1,27 +1,24 @@
-import { createSignal } from "solid-js";
+import { createMemo, createSignal } from "solid-js";
 
 export function createDnd(setFile: (blob: Blob) => void) {
-  const [isDragOver, setIsDraggedOver] = createSignal(false);
   const [counter, setCounter] = createSignal(0);
+  const isDragOver = createMemo(() => counter() > 0);
 
   function onDrop(e: DragEvent) {
     e.preventDefault();
     const file = e.dataTransfer?.files[0];
     if (file && file.type.startsWith("image/")) {
       setFile(file);
-      setIsDraggedOver(false);
       setCounter(0);
     }
   }
 
   function onDragEnter() {
     setCounter((prev) => prev + 1);
-    if (counter() === 1) setIsDraggedOver(true);
   }
 
   function onDragLeave() {
     setCounter((prev) => prev - 1);
-    if (counter() === 0) setIsDraggedOver(false);
   }
 
   function onDragOver(e: DragEvent) {
