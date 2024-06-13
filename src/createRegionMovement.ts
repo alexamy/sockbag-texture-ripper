b71bc877de84b14f156d90765661e551b70803c4
Aleksandr Chashchin <achasch@gmail.com>
2024-06-13 10:17:24 +0200

    turn off origin in styles

--- src/createRegionMovement.ts
+++ src/createRegionMovement.ts
@@ -22,12 +22,12 @@ export function createRegionMovement() {
 
   const style = createMemo(() => {
     const scaled = v.scale(origin(), scale() - 1);
-    const shift = `translate(${scaled.x}px, ${scaled.y}px)`;
+    const shift = ""; //`translate(${scaled.x}px, ${scaled.y}px)`;
     const move = `translate(${translate().x}px, ${translate().y}px)`;
     const zoom = `scale(${scale()})`;
 
     const transform = `${move} ${shift} ${zoom}`;
-    const transformOrigin = `${origin().x}px ${origin().y}px`;
+    const transformOrigin = ""; //`${origin().x}px ${origin().y}px`;
 
     return {
       transform,
