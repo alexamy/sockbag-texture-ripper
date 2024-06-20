733a97ad52b4b93ece0931d9d506f906af1c31bb
Alexander Chaschin <ac@happynumbers.com>
2024-06-20 07:40:26 +0200

    add macaron

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { macaronVitePlugin } from "@macaron-css/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
   base: "/sockbag-texture-ripper/",
+  plugins: [solid(), macaronVitePlugin()],
 });
