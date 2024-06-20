c1cc7bec7d546e6fda4e99645753dedea88abec0
Alexander Chaschin <ac@happynumbers.com>
2024-06-20 19:16:55 +0200

    add upload click

--- src/Upload.tsx
+++ src/Upload.tsx
@@ -1,5 +1,30 @@
+import { createSignal } from "solid-js";
 import { Button } from "./styles";
 
 export function Upload() {
-  return <Button>Upload</Button>;
+  const [input, setInput] = createSignal<HTMLInputElement>();
+
+  function show() {
+    input()?.click();
+  }
+
+  function upload() {
+    const file = input()?.files?.[0];
+    if (file) {
+      console.log(file);
+    }
+  }
+
+  return (
+    <>
+      <Button onClick={show}>Upload</Button>
+      <input
+        ref={setInput}
+        type="file"
+        accept="image/*"
+        hidden
+        onChange={upload}
+      />
+    </>
+  );
 }
