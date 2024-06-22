5cd13fe925353486c58b84e1768b95fad281b59a
Alexander Chaschin <ac@happynumbers.com>
2024-06-22 18:04:37 +0200

    add texture case

--- tests/texture.spec.ts
+++ tests/texture.spec.ts
@@ -1,5 +1,27 @@
 import { test } from "@playwright/test";
+import { AppPage } from "./utils/app";
 
-test("texture", async ({ page }) => {
+test("gap", async ({ page }) => {
+  const app = new AppPage(page);
+  await app.goto();
+
+  // upload image
+  await app.upload("./images/cat.png", import.meta.url);
+  // draw first quad
+  await app.editor.content.click({ position: { x: 25, y: 60 } });
+  await app.editor.content.click({ position: { x: 90, y: 81 } });
+  await app.editor.content.click({ position: { x: 94, y: 168 } });
+  await app.editor.content.click({ position: { x: 47, y: 150 } });
+  // draw second quad
+  await app.editor.content.click({ position: { x: 185, y: 100 } });
+  await app.editor.content.click({ position: { x: 320, y: 67 } });
+  await app.editor.content.click({ position: { x: 322, y: 214 } });
+  await app.editor.content.click({ position: { x: 166, y: 148 } });
+
+  // see the result without gap
+  await app.texture.toHaveScreenshot();
   // increase the gap
+  await app.inputs.gap.fill("10");
+  // see the result with gap
+  await app.texture.toHaveScreenshot();
 });
