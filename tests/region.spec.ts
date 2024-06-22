56ed48fa3d5aa87283c1ac64591dacfaf573d819
Alexander Chaschin <ac@happynumbers.com>
2024-06-22 17:48:51 +0200

    add comment

--- tests/region.spec.ts
+++ tests/region.spec.ts
@@ -3,9 +3,11 @@ import { test } from "@playwright/test";
 test("controls the editor region", async ({ page }) => {
   // pan
   // zoom
+  // reset
 });
 
 test("controls the texture region", async ({ page }) => {
   // pan
   // zoom
+  // reset
 });
