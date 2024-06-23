21555d33f69dbdcac69ca6de24899ff73bcc5300
Alexander Chaschin <ac@happynumbers.com>
2024-06-23 23:55:49 +0200

    add drag test

--- tests/editor.spec.ts
+++ tests/editor.spec.ts
@@ -36,3 +36,37 @@ test("discards last draw point by right click", async ({ page }) => {
   });
   await app.editor.toHaveScreenshot();
 });
+
+test("drags quad", async ({ page }) => {
+  const app = new AppPage(page);
+  await app.goto();
+
+  // upload image
+  await app.upload("./images/cat.png", import.meta.url);
+  await app.editor.toHaveScreenshot();
+
+  // draw first quad
+  await app.editor.content.click({ position: { x: 25, y: 60 } });
+  await app.editor.content.click({ position: { x: 90, y: 81 } });
+  await app.editor.content.click({ position: { x: 94, y: 168 } });
+  await app.editor.content.click({ position: { x: 47, y: 150 } });
+
+  // draw second quad
+  await app.editor.content.click({ position: { x: 185, y: 100 } });
+  await app.editor.content.click({ position: { x: 320, y: 67 } });
+  await app.editor.content.click({ position: { x: 322, y: 214 } });
+  await app.editor.content.click({ position: { x: 166, y: 148 } });
+  await app.editor.toHaveScreenshot();
+
+  // drag the first quad
+  await page.mouse.move(55, 70);
+  await page.mouse.down();
+  await page.mouse.move(65, 80);
+  await page.mouse.move(75, 90);
+  await page.mouse.move(85, 100);
+  await page.mouse.move(95, 110);
+  await page.mouse.move(105, 120);
+  await page.mouse.move(115, 130);
+  await page.mouse.up();
+  await app.editor.toHaveScreenshot();
+});
