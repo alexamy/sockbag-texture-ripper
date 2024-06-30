aac2d32f8ba746affdac434c4857c6fadc0de435
Alexander Chaschin <ac@happynumbers.com>
2024-06-30 13:00:56 +0200

    add stoolbar

--- stories/Root.tsx
+++ stories/Root.tsx
@@ -1,5 +1,6 @@
 import { createResize } from "@/hooks/createResize";
 import { style } from "@macaron-css/core";
+import { styled } from "@macaron-css/solid";
 import { For, createSignal } from "solid-js";
 import { Story, stories } from "./stories";
 
@@ -23,10 +24,25 @@ const sSeparator = style({
 });
 
 const sToolbar = style({
+  display: "flex",
+  alignItems: "center",
+  gap: 10,
   padding: "0.5rem 1rem",
-  gridColumn: "1 / -1",
   borderBottom: "2px solid grey",
   fontWeight: "bold",
+});
+
+const Header = styled("h1", {
+  base: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+  },
+});
+
+const Button = styled("button", {
+  base: {
+    padding: 4,
+  },
 });
 
 const sList = style({
@@ -56,7 +72,10 @@ export function Root() {
 
   return (
     <div class={sRoot}>
-      <div class={sToolbar}>Stories</div>
+      <div class={sToolbar}>
+        <Header>Stories</Header>
+        <Button>Switch background</Button>
+      </div>
       <div class={sMain}>
         <ul class={sList} style={{ width: `${resize.left()}%` }}>
           <For each={stories}>
